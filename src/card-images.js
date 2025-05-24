8002e2488e11da7aed22fc86c3751798ed0eb797
lawrence-ruscia <140231442+lawrence-ruscia@users.noreply.github.com>
2025-05-25 02:25:05 +0800

    feat: increase cards fetched from 6 to 12

--- src/card-images.js
+++ src/card-images.js
@@ -8,7 +8,7 @@ export const getCards = async () => {
 };
 
 const fetchData = async () => {
-  const response = await fetch('https://nekos.best/api/v2/neko?amount=6');
+  const response = await fetch('https://nekos.best/api/v2/neko?amount=12');
   const data = await response.json();
   const results = data.results;
 
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,15 @@
+import { useState } from 'react';
 import styles from './Card.module.css';
 export const Card = ({ url, handleClick }) => {
+  const [loaded, setLoaded] = useState(false);
   return (
     <div className={styles.container} onClick={handleClick}>
-      <img className={styles.image} src={url} />
+      {!loaded && <div>Loading...</div>}
+      <img
+        className={`${loaded ? styles.loaded : styles.image}`}
+        src={url}
+        onLoad={() => setLoaded(true)}
+      />
     </div>
   );
 };
